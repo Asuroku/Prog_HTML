8751f462b4734fbf9a4e3687e834b76d080aab4b
Asuroku <79782417+Asuroku@users.noreply.github.com>
2021-03-14 13:19:24 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 import random
+import os
 
+summ = 0
 
 for i in range(20):
-    f = open(f"{i}", "w")
+    f = open(f"BD/{random.randint(850, 950)}", "w")
     f.write(f'{random.randint(1, 1000)}')
     f.close()
+for i in os.walk("BD"):
+    for j in i[2]:
+        f = open(f"BD/{j}", "r")
+        a = f.read()
+        summ += int(a)
+        f.close()
+        print(a)
+        os.remove(f"BD/{j}")
+print(summ)
